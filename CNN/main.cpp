cc37cb4512d07b3d496e265ea63ee6f0d8dde0d2
linmude <791283555@qq.com>
2014-08-01 19:53:23 +0800

    Initial Commit

---
+++ CNN/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CNN
+//
+//  Created by Eric on 8/1/14.
+//  Copyright (c) 2014 cv. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
